0e78a37aa2ecca3d9559e6813ea833fadfe4126c
AzharSultan <azh.sultan@gmail.com>
2017-09-12 23:52:28 +0200

    Update AppointmentSearch_1min.py

--- AppointmentSearch_1min.py
+++ AppointmentSearch_1min.py
@@ -160,7 +160,7 @@ class AppointmentSearch():
                 hr = hms.hour
                 mint = hms.minute
                 sec = hms.second
-                while not (hr == 21 and mint >= 51 and mint <53):
+                while not (hr == 21 and mint >= 54 and mint <56):
                     hms = datetime.now()
                     hr = hms.hour
                     mint = hms.minute
